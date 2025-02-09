26749d49e4c470e99206b8d352c22a5a7abb878d
Carloscv200313 <carlosscv200313@gmail.com>
2025-02-08 23:03:46 -0500

    dandole estilos al apartado de contactos

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -21,106 +21,108 @@ export default function Portfolio() {
 
   return (
     <div className="container mx-auto px-4 py-8 bg-transparent text-text-primary">
-    <TextAnimate animation="slideLeft" by="character" 
-          className="text-4xl lg:text-7xl font-bold mb-10 text-center text-accent-blue">
-      Mis Proyectos
-    </TextAnimate>
-    <div className="flex justify-center space-x-4 mb-8">
-      <Button
-        variant={filter === "all" ? "default" : "outline"}
-        onClick={() => setFilter("all")}
-        className={`${
-          filter === "all" ? "bg-accent-blue text-bg-dark" : "text-text-secondary border-text-secondary"
-        } hover:bg-accent-blue hover:text-bg-dark transition-colors duration-300`}
+      <TextAnimate animation="slideLeft" by="character"
+        className="text-4xl lg:text-7xl font-bold mb-10 text-center text-accent-blue">
+        Mis Proyectos
+      </TextAnimate>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+        className="flex justify-center md:space-x-4 space-x-0 mb-8"
       >
-        Todos
-      </Button>
-      {categories.map((category) => (
         <Button
-          key={category}
-          variant={filter === category ? "default" : "outline"}
-          onClick={() => setFilter(category)}
-          className={`${
-            filter === category ? "bg-accent-blue text-bg-dark" : "text-text-secondary border-text-secondary"
-          } hover:bg-accent-blue hover:text-bg-dark transition-colors duration-300`}
+          variant={filter === "all" ? "default" : "outline"}
+          onClick={() => setFilter("all")}
+          className={`${filter === "all" ? "bg-accent-blue text-bg-dark" : "text-text-secondary border-text-secondary"
+            } hover:bg-accent-blue hover:text-bg-dark transition-colors duration-300`}
         >
-          {category.charAt(0).toUpperCase() + category.slice(1)}
+          Todos
         </Button>
-      ))}
-    </div>
-
-    <motion.div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-4" layout>
-      <AnimatePresence>
-        {filteredProjects.map((project) => (
-          <motion.div
-            key={project.id}
-            layout
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0, scale: 0.8 }}
-            transition={{ duration: 0.5 }}
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-          >
-            <Card
-              className="cursor-pointer h-full overflow-hidden bg-bg-light border-text-secondary hover:border-accent-blue transition-colors duration-300"
-              onClick={() => setSelectedProject(project)}
-            >
-              <CardContent className="p-4 ">
-                <Image
-                  src={project.image || "/placeholder.svg"}
-                  alt={project.title}
-                  height={500}
-                  width={1000}
-                  className="w-full h-64 object-cover rounded-md mb-4 transition-transform duration-300 ease-in-out transform hover:scale-110"
-                />
-                <h3 className="text-xl font-semibold mb-2 text-accent-blue">{project.title}</h3>
-                <p className="text-sm text-text-secondary line-clamp-2">{project.description}</p>
-                <Badge className="mt-2 bg-accent-green text-bg-dark">{project.category}</Badge>
-              </CardContent>
-            </Card>
-          </motion.div>
-        ))}
-      </AnimatePresence>
-    </motion.div>
-
-    <Dialog open={!!selectedProject} onOpenChange={() => setSelectedProject(null)}>
-      {selectedProject && (
-        <DialogContent className="max-w-[340px] md:max-w-[500px] max-h-[calc(100vh-70px)] overflow-y-auto bg-bg-light text-text-primary border-text-secondary">
-          <DialogHeader>
-            <DialogTitle className="text-accent-blue">{selectedProject.title}</DialogTitle>
-          </DialogHeader>
-          <Image
-            src={selectedProject.image || "/placeholder.svg"}
-            alt={selectedProject.title}
-            height={300}
-            width={500}
-            className="w-full h-56 object-cover rounded-md mb-4"
-          />
-          <DialogDescription className="text-text-secondary">{selectedProject.description}</DialogDescription>
-          <div className="flex flex-wrap gap-2 my-4">
-            {selectedProject.languages.map((lang) => (
-              <Badge
-                key={lang}
-                variant="secondary"
-                className="bg-gradient-to-r from-blue-400 to-green-600 text-bg-dark"
-              >
-                {lang}
-              </Badge>
-            ))}
-          </div>
+        {categories.map((category) => (
           <Button
-            asChild
-            className="w-full bg-accent-blue text-bg-dark hover:bg-accent-green transition-colors duration-300"
+            key={category}
+            variant={filter === category ? "default" : "outline"}
+            onClick={() => setFilter(category)}
+            className={`${filter === category ? "bg-accent-blue text-bg-dark" : "text-text-secondary border-text-secondary"
+              } hover:bg-accent-blue hover:text-bg-dark transition-colors duration-300`}
           >
-            <a href={selectedProject.url} target="_blank" rel="noopener noreferrer">
-              Ver Proyecto <ExternalLink className="ml-2 h-4 w-4" />
-            </a>
+            {category.charAt(0).toUpperCase() + category.slice(1)}
           </Button>
-        </DialogContent>
-      )}
-    </Dialog>
-  </div>
+        ))}
+      </motion.div>
+      <motion.div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-4" layout>
+        <AnimatePresence>
+          {filteredProjects.map((project) => (
+            <motion.div
+              key={project.id}
+              layout
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.8 }}
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              <Card
+                className="cursor-pointer h-full overflow-hidden bg-bg-light border-text-secondary hover:border-accent-blue transition-colors duration-300"
+                onClick={() => setSelectedProject(project)}
+              >
+                <CardContent className="p-4 ">
+                  <Image
+                    src={project.image || "/placeholder.svg"}
+                    alt={project.title}
+                    height={500}
+                    width={1000}
+                    className="w-full h-64 object-cover rounded-md mb-4 transition-transform duration-300 ease-in-out transform hover:scale-110"
+                  />
+                  <h3 className="text-xl font-semibold mb-2 text-accent-blue">{project.title}</h3>
+                  <p className="text-sm text-text-secondary line-clamp-2">{project.description}</p>
+                  <Badge className="mt-2 bg-accent-green text-bg-dark">{project.category}</Badge>
+                </CardContent>
+              </Card>
+            </motion.div>
+          ))}
+        </AnimatePresence>
+      </motion.div>
+
+      <Dialog open={!!selectedProject} onOpenChange={() => setSelectedProject(null)}>
+        {selectedProject && (
+          <DialogContent className="max-w-[340px] md:max-w-[500px] max-h-[calc(100vh-70px)] overflow-y-auto bg-bg-light text-text-primary border-text-secondary">
+            <DialogHeader>
+              <DialogTitle className="text-accent-blue">{selectedProject.title}</DialogTitle>
+            </DialogHeader>
+            <Image
+              src={selectedProject.image || "/placeholder.svg"}
+              alt={selectedProject.title}
+              height={300}
+              width={500}
+              className="w-full h-56 object-cover rounded-md mb-4"
+            />
+            <DialogDescription className="text-text-secondary">{selectedProject.description}</DialogDescription>
+            <div className="flex flex-wrap gap-2 my-4">
+              {selectedProject.languages.map((lang) => (
+                <Badge
+                  key={lang}
+                  variant="secondary"
+                  className="bg-gradient-to-r from-blue-400 to-green-600 text-bg-dark"
+                >
+                  {lang}
+                </Badge>
+              ))}
+            </div>
+            <Button
+              asChild
+              className="w-full bg-accent-blue text-bg-dark hover:bg-accent-green transition-colors duration-300"
+            >
+              <a href={selectedProject.url} target="_blank" rel="noopener noreferrer">
+                Ver Proyecto <ExternalLink className="ml-2 h-4 w-4" />
+              </a>
+            </Button>
+          </DialogContent>
+        )}
+      </Dialog>
+    </div>
   )
 }
 
--- src/components/contacto-oscuro-mejorado.tsx
+++ src/components/contacto-oscuro-mejorado.tsx
@@ -1,95 +1,107 @@
 "use client"
 import emailjs from '@emailjs/browser';
 import { useRef } from "react"
-import { Send, User, Mail, MessageSquare } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import Image from "next/image"
+import { Send, User, Mail, MessageSquare, PhoneCall } from "lucide-react";
 export default function ContactoOscuroMejorado() {
   const form = useRef<HTMLFormElement | null>(null);
+
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
+    e.preventDefault();
     if (form.current) {
       emailjs
         .sendForm('service_aeyw9ha', 'template_4v7rme2', form.current, 'sXPhw8Cvp4MSi3Kls')
-        .then(
-          () => {
-            console.log('SUCCESS!');
-            form.current?.reset();
-          },
-          (error: { text: unknown; }) => {
-            console.log('FAILED...', error.text);
-          },
-        );
+        .then(() => {
+          console.log('SUCCESS!');
+          form.current?.reset();
+        })
+        .catch((error) => console.log('FAILED...', error));
     }
-  }
+  };
 
   return (
-    <div className="min-h-[70vh] flex items-center justify-center  p-4">
-      <Card className="w-full max-w-5xl bg-white/ backdrop-blur-sm text-accent-blue shadow-lg shadow-accent-blue/20">
-        <div className="flex flex-col md:flex-row">
-          <div className="md:w-1/2 p-6 flex items-center justify-center">
-            <div className="relative w-52 h-52 md:w-64 md:h-64 lg:w-72 lg:h-72 rounded-full ring-4 ring-blue-600  overflow-hidden">
-              <Image
-                src="/fondo.jpg"
-                alt="Profile picture"
-                fill
-                className="object-cover"
+    <div 
+      className="min-h-[80vh] flex items-center justify-center p-4 relative bg-fixed bg-no-repeat bg-center"
+      style={{ backgroundImage: "url('/fondo.jpg')" }}
+    >
+      {/* Overlay oscuro */}
+      <div className="absolute inset-0 bg-black/85"></div>
+
+      <Card className="w-full max-w-5xl bg-gray-900/80 backdrop-blur-md text-white shadow-lg relative z-10 p-0">
+        <CardHeader>
+          <CardTitle className="text-4xl lg:text-5xl font-bold text-center">
+            Contáctame
+          </CardTitle>
+        </CardHeader>
+        {/* Descripción Motivadora */}
+        <p className="text-center text-base text-gray-300 px-4 mb-6">
+          🚀 <strong>¿Buscas a alguien comprometido, apasionado y listo para nuevos desafíos?</strong>  
+          Estoy disponible para trabajar en proyectos emocionantes y colaborar en soluciones innovadoras.  
+          Ya sea que tengas una idea en mente, un reto que enfrentar o simplemente quieras conectar,  
+          estaré encantado de escuchar y contribuir con mi experiencia.  
+          <br /> <strong>¡Hablemos y hagamos que las cosas sucedan!</strong>  
+        </p>
+
+        <CardContent>
+          <form ref={form} onSubmit={handleSubmit} className="space-y-4 ">
+            <div className="relative">
+              <User className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white/50" size={18} />
+              <Input
+                type="text"
+                name="name"
+                placeholder="Nombre"
+                required
+                className="pl-10 bg-gray-800 border-gray-700 placeholder-white/50 text-white focus:border-accent-green"
               />
             </div>
-          </div>
-          <div className="md:w-1/2">
-            <CardHeader>
-              <CardTitle className="text-4xl lg:text-7xl font-bold text-center text-accent-blue">Contáctame</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <form ref={form} onSubmit={handleSubmit} className="space-y-4">
-                <div className="relative">
-                  <User className="absolute left-3 top-1/2 transform -translate-y-1/2 text-accent-blue/50" size={18} />
-                  <Input
-                    type="text"
-                    name="name"
-                    placeholder="Nombre"
-                    required
-                    className="pl-10 bg-gray-800 border-gray-700 placeholder-accent-blue/50 text-accent-blue focus:border-accent-green"
-                  />
-                </div>
-                <div className="relative">
-                  <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-accent-blue/50" size={18} />
-                  <Input
-                    type="email"
-                    name="email"
-                    placeholder="Correo electrónico"
-                    required
-                    className="pl-10 bg-gray-800 border-gray-700 placeholder-accent-blue/50 text-accent-blue focus:border-accent-green"
-                  />
-                </div>
-                <div className="relative">
-                  <MessageSquare className="absolute left-3 top-3 text-accent-blue/50" size={18} />
-                  <Textarea
-                    placeholder="Tu mensaje"
-                    name="mensaje"
-                    required
-                    className="pl-10 bg-gray-800 border-gray-700 placeholder-accent-blue/50 text-accent-blue focus:border-accent-green"
-                    rows={4}
-                  />
-                </div>
-                <Button
-                  type="submit"
-                  className={`w-full bg-accent-green hover:bg-accent-green/80 text-black font-semibold transition-all duration-300 ease-in-out transform hover:scale-105`}
+            <div className="relative">
+              <Mail className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white/50" size={18} />
+              <Input
+                type="email"
+                name="email"
+                placeholder="Correo electrónico"
+                required
+                className="pl-10 bg-gray-800 border-gray-700 placeholder-white/50 text-white focus:border-accent-green"
+              />
+            </div>
+            <div className="relative">
+              <MessageSquare className="absolute left-3 top-3 text-white/50" size={18} />
+              <Textarea
+                placeholder="Tu mensaje"
+                name="mensaje"
+                required
+                className="pl-10 bg-gray-800 border-gray-700 placeholder-white/50 text-white focus:border-accent-green"
+                rows={4}
+              />
+            </div>
+            <Button
+              type="submit"
+              className="w-full bg-accent-green hover:bg-accent-green/80 text-black font-semibold transition-all duration-300 ease-in-out transform hover:scale-105"
+            >
+              Enviar
+              <Send className="ml-2 h-5 w-5" />
+            </Button>
+          </form>
+        </CardContent>
 
-                >
-                  Enviar
-                  <Send className="ml-2 h-5 w-5" />
-                </Button>
-              </form>
-            </CardContent>
+        {/* 📞 Iconos de Contacto */}
+        <div className="my-6 text-center">
+          <p className="text-lg font-semibold text-gray-300 mb-2">📲 También puedes contactarme aquí:</p>
+          <div className="flex md:flex-row flex-col justify-center items-center  gap-6">
+            <div className="flex items-center gap-2 text-gray-300">
+              <PhoneCall size={20} className="text-accent-green" />
+              <span className="text-lg">922 814 060</span>
+            </div>
+            <div className="flex items-center gap-2 text-gray-300">
+              <Mail size={20} className="text-accent-green" />
+              <span className="text-lg">carlosscv200313@gmail.com</span>
+            </div>
           </div>
         </div>
       </Card>
     </div>
-  )
+  );
 }
-
