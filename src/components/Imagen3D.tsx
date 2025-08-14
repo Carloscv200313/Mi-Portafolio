88fc6bcd67b0b2e0c949e7a2ce803f86eb9fe655
Carloscv200313 <carlosscv200313@gmail.com>
2025-08-14 00:29:15 -0500

    feat: tratando de mejorar la carga lenta de la imagen 3D V2.0

--- src/components/Imagen3D.tsx
+++ src/components/Imagen3D.tsx
@@ -1,17 +1,11 @@
 "use client";
-import dynamic from "next/dynamic";
-const SplineViewer = dynamic(
-  () => import("./SplineViewer").then((mod) => mod.default), 
-  {
-    ssr: false,
-    loading: () => <div className="w-full h-full flex items-center justify-center">Cargando...</div>
-  }
-);
+import SplineViewer from "./SplineViewer";
+
 export const Imagen3D = () => {
   return (
     <div className="w-full xl:h-screen lg:h-[50vh] h-[60vh] flex items-center justify-center xl:-translate-y-20 md:-translate-y-32">
       <div className="md:w-[1000px] md:h-[900px] w-[500px] h-[500px] flex items-center justify-center">
-        <SplineViewer/>
+        <SplineViewer />
       </div>
     </div>
   );
