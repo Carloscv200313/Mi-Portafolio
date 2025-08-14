2f6e9916ca0348eae67b1951f7ee1f553bd124d8
Carloscv200313 <carlosscv200313@gmail.com>
2025-08-14 01:27:27 -0500

    feat: quitando un archivo tailwind

--- src/components/Imagen3D.tsx
+++ src/components/Imagen3D.tsx
@@ -10,7 +10,7 @@ const SplineLocal = dynamic(() => import("./SplineViewer"), {
 });
 export const Imagen3D = () => {
   return (
-    <div className="w-full xl:h-screen lg:h-[50vh] h-[60vh] flex items-center justify-center xl:-translate-y-20 md:-translate-y-32">
+    <div className="w-full xl:h-screen lg:h-[10vh] h-[60vh] flex items-center justify-center xl:-translate-y-20 md:-translate-y-32">
       <div className="md:w-[1000px] md:h-[900px] w-[500px] h-[500px] flex items-center justify-center">
         <SplineLocal
           scene="https://prod.spline.design/nC8mML7yKDFE4yTp/scene.splinecode"
